5750d49cb1094e0b4da33778aa5aa82ac821c1a2
shivam kumar chauhan <shivamchauhan3596@gmail.com>
2016-08-26 02:58:05 +0530

    some changes

--- final.py
+++ final.py
@@ -2,23 +2,28 @@ import socket
 import cv2
 import numpy as np
 import math
+import time
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 host = '127.0.0.1'
 port = 8888
-
  
 while True:
     ret, frame= cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (15,15), 0 )
-    
+    cv2.rectangle(frame,(300,300),(100,100),(0,255,0),0)
+    crop_img = frame[100:300, 100:300]
+    gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (35, 35), 0 )
+
+    _, thresh1 = cv2.threshold(blur, 100, 255,
+                               cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    cv2.imshow('Thresholded', thresh1)
     #gaus = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,115,1) 
-    _ ,otsu = cv2.threshold(blur,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #_ ,otsu = cv2.threshold(blur,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
     
-    _, contours, hierarchy = cv2.findContours(otsu.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    _, contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #CHAIN_APPROX_SIMPLE
     max_area = -1
     l = len(contours)
     for i in range(l):
@@ -29,18 +34,18 @@ while True:
             ci=i
     cnt = contours[ci]
     x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 0)
+    cv2.rectangle(crop_img, (x,y), (x+w,y+h), (0,0,255), 0)
     hull = cv2.convexHull(cnt)
-    drawing = np.zeros(frame.shape,np.uint8)
+    drawing = np.zeros(crop_img.shape,np.uint8)
     cv2.drawContours(drawing, [cnt],0 ,(0,255,0),0)
     cv2.drawContours(drawing, [hull], 0 ,(0,255,0), 0)
     hull = cv2.convexHull(cnt, returnPoints = False)
     defects = cv2.convexityDefects(cnt, hull)
-    count_defects =0
-    cv2.drawContours(otsu, contours, -3, (0,255,0), 3)
+    count_defects = -1
+    cv2.drawContours(thresh1, contours, -1, (0,255,0), 5)
     
     for i in range(defects.shape[0]):
-        s,e,f,d = defects[i,0]
+        s,e,f,d = defects[i,0]    #[ start point, end point, farthest point, approximate distance to farthest point ]
         start = tuple(cnt[s][0])
         end = tuple(cnt[e][0])
         far = tuple(cnt[f][0])
@@ -50,12 +55,10 @@ while True:
         angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
         if angle <= 90:
             count_defects += 1
-            cv2.circle(frame,far,1,[0,0,255],-1)
-        cv2.line(frame,start,end,[0,255,0],2)
+            cv2.circle(crop_img,far,1,[0,0,255],-1)
+        cv2.line(crop_img,start,end,[0,255,0],2)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX  
-
-    s= socket.socket()
+    s = socket.socket()
     s.connect((host,port))
     if count_defects == 1:
         message ="open http://www.oldstuff.in"
@@ -74,18 +77,15 @@ while True:
         print(message)
         s.send(message)
     else:
-        message ="open http://google.com"
+        message ="waiting for instructions..."
         print(message)
-        s.send(message)
 
     
 #Output display of the frames
-    cv2.imshow('vid',frame)
-    #cv2.imshow('gray', gray)
-    #cv2.imshow('blur', blur)
-    cv2.imshow('otsu thresh',otsu)
-    #cv2.imshow('gaus',gaus)
     cv2.imshow('drwaing', drawing)
+    cv2.imshow('vid',frame)    
+    cv2.imshow('vid',crop_img)
+    #cv2.imshow('gaus',gaus)
 #Hold windows unitill q key is pressed  
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -94,4 +94,3 @@ s.close()
 
 cap.release()
 cv2.destroyAllWindows()
-
